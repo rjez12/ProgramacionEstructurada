4f66aeeea41c5bd12cb836415bb6b307c0f6f1b2
rjez12 <jesserjrch@uamv.edu.ni>
2025-09-22 02:23:09 -0600

    Paso de valor o referencia

---
+++ Montos/Montos/Dao/Calculo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Montos.Dao
+{
+    public static class Calculo
+    {
+        /// <summary>
+        /// Calcular monto por paso de valor
+        /// </summary>
+        /// <param name="precio"></param>
+        /// <returns></returns>
+        public static double calcularmonto(double precio)
+        {
+            precio *= 1.15;
+            return precio;
+        }
+        
+        /// <summary>
+        /// Calcular monto por paso de referencia
+        /// </summary>
+        /// <param name="precio"></param>
+        /// <returns></returns>
+        public static double CalcularMonto( ref double precio)
+        {
+            precio *= 1.15;
+            return precio;
+        }
+
+    }
+}
---
+++ Montos/Montos/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Montos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPrecio = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblMonto = new System.Windows.Forms.Label();
+            this.btnCalcularRef = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Precio:";
+            // 
+            // tbPrecio
+            // 
+            this.tbPrecio.Location = new System.Drawing.Point(120, 40);
+            this.tbPrecio.Name = "tbPrecio";
+            this.tbPrecio.Size = new System.Drawing.Size(152, 20);
+            this.tbPrecio.TabIndex = 1;
+            this.tbPrecio.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(120, 66);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(152, 54);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular monto con IVA";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblMonto
+            // 
+            this.lblMonto.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMonto.Location = new System.Drawing.Point(67, 224);
+            this.lblMonto.Name = "lblMonto";
+            this.lblMonto.Size = new System.Drawing.Size(205, 78);
+            this.lblMonto.TabIndex = 3;
+            this.lblMonto.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCalcularRef
+            // 
+            this.btnCalcularRef.Location = new System.Drawing.Point(120, 135);
+            this.btnCalcularRef.Name = "btnCalcularRef";
+            this.btnCalcularRef.Size = new System.Drawing.Size(152, 54);
+            this.btnCalcularRef.TabIndex = 4;
+            this.btnCalcularRef.Text = "Calcular monto con IVA - Ref";
+            this.btnCalcularRef.UseVisualStyleBackColor = true;
+            this.btnCalcularRef.Click += new System.EventHandler(this.btnCalcularRef_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 388);
+            this.Controls.Add(this.btnCalcularRef);
+            this.Controls.Add(this.lblMonto);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.tbPrecio);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Monto Con IVA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPrecio;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblMonto;
+        private System.Windows.Forms.Button btnCalcularRef;
+    }
+}
+
